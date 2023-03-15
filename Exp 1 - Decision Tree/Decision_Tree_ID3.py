4a5027581967aafb1164c38039d765b307b42dd6
madhumitha105 <91081247+madhumitha105@users.noreply.github.com>
2023-03-15 19:46:18 +0530

    Add files via upload

---
+++ Exp 1 - Decision Tree/Decision_Tree_ID3.py
@@ -0,0 +1,54 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+data = pd.read_csv("C:/Users/madhu/Downloads/ML_all_exp/ML_Lab/Exp 1 - Decision Tree/Dataset/Social_Network_Ads.csv")
+data.head()
+
+feature_cols = ['Age', 'EstimatedSalary']
+X = data.iloc[:, [2, 3]].values
+y = data.iloc[:, 4].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
+from sklearn.tree import DecisionTreeClassifier
+classifier = DecisionTreeClassifier(criterion='entropy')
+classifier = classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+# Accuracy
+from sklearn import metrics
+print('Accuracy Score:', metrics.accuracy_score(y_test, y_pred))
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min()-1, stop=X_set[:, 0].max()+1, step=0.01), np.arange(start=X_set[:, 1].min()-1, stop= X_set[:, 1].max()+1, step=0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape), alpha=0.75, cmap=ListedColormap(("red", "green")))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c=ListedColormap(("red", "green"))(i), label=j)
+    plt.title("Decision Tree(Test set)")
+    plt.xlabel("Age")
+    plt.ylabel("Estimated Salary")
+    plt.legend()
+    plt.show()
+
+from sklearn.tree import export_graphviz
+from six import StringIO
+from IPython.display import Image
+import pydotplus
+
+dot_data = StringIO()
+export_graphviz(classifier, out_file=dot_data, filled=True, rounded=True, special_characters=True, feature_names=feature_cols, class_names=['0', '1'])
+graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
+Image(graph.write_png("Decision_Tree_ID3.png"))
